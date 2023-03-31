07923f967f0b1e4b490e3c93913efb4560cb660d
Michael Change Chen <michaelexgt@gmail.com>
2023-03-31 18:42:33 +0800

    add loading page

---
+++ src/components/view/AirPortLoader.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="page-loading">
+		<div class="loader">
+		</div>
+		<p>Loading...</p>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	@import '@/components/css/air-port-loader.scss';
+</style>
